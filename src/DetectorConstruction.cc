e4da312a2a387b6ad8c2e76634c1b59eb41a773e
Abhijit Bhattacharyya <abhihere@gmail.com>
2017-05-30 17:33:57 +0530

    Modification for geometry only. code is running to check replica and overlaps......

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -32,9 +32,9 @@ G4ThreadLocal
 
 // The Constructor
 DetectorConstruction::DetectorConstruction() : G4VUserDetectorConstruction(),
-                                                   fAbsorberPV(0),
-                                                   fTargetPV(0),
-                                                   fCheckOverlaps(true)
+					       fAbsorberPV(0),
+					       fTargetPV(0),
+					       fCheckOverlaps(true)
 {
 }
 
@@ -46,92 +46,107 @@ DetectorConstruction::~DetectorConstruction()
 // Construct
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {
-    // Define Materials
-    DefineMaterials();
+  // Define Materials
+  DefineMaterials();
 
-    // Define Volumes
-    return DefineVolumes();
+  // Define Volumes
+  return DefineVolumes();
 }
 
 // Now define Material in a function
 void DetectorConstruction::DefineMaterials()
 {
-    G4Nistmanager* nistManager = G4NistManager::Instance();
+  G4NistManager* nistManager = G4NistManager::Instance();
 
-    // Select Lead 100% pure
-    G4Material* Pb             = nistManager->FindOrBuildMaterial("G4_Pb);
-    G4Material* Be             = nistManager->FindOrBuildMaterial("G4_Be");
-    G4Material* polyFilm       = nistManager->FindOrBuidlMaterial("G4_KAPTON");
-    G4Material* steel          = nistManager->FindOrBuildMaterial("G4_Stainless-Steel");
-    G4Material* Air            = nistManager->FindOrBuildMaterial("G4_AIR");
-    G4Material* Argongas       = nistManager->FindOrBuildMaterial("G4_Ar");
-    G4Material* Vac            = nistmanager->FindOrBuildMaterial("G4_Galactic");
-    G4Material* defaultMat     = nistManager->FindOrBuildMaterial("G4_Galactic");        
+  G4double a;  // mass of a mole;
+  G4double z;  // z=mean number of protons;  
+  G4double density; 
 
-    // Print the material synthesized
-    G4cout << G4endl << "The materials defined are : " << G4endl << G4endl;
-    G4cout << *(G4Material::GetMaterialTable()) << G4endl;
+  nistManager->FindOrBuildMaterial("G4_Pb");
+  nistManager->FindOrBuildMaterial("G4_Be");
+  nistManager->FindOrBuildMaterial("G4_KAPTON");
+  nistManager->FindOrBuildMaterial("G4_Stainless-Steel");
+  nistManager->FindOrBuildMaterial("G4_AIR");
+  nistManager->FindOrBuildMaterial("G4_Ar");
+  nistManager->FindOrBuildMaterial("G4_Galactic");
+
+
+  // Print the material synthesized
+  G4cout << G4endl << "The materials defined are : " << G4endl << G4endl;
+  G4cout << *(G4Material::GetMaterialTable()) << G4endl;
 }
 
-// Defien Volumes
+// Define Volumes
 G4VPhysicalVolume* DetectorConstruction::DefineVolumes()
 {
-    // Geometry Parameters
-    G4double brickLength    = 20.0*cm;
-    G4double brickDepth     = 10.0*cm;
-    G4double brickHeight    = 5.0*cm;
-    G4double brickXY        = (brickLength * brickheight);
-    G4double brickVolume    = (brickXY * brickDepth); 
-    G4double PbArrayLength  = 150.0*cm;
-    G4double PbArrayDepth   = 150.0*cm;
-    G4double PbArrayHeight  = 150.0*cm;
-    G4double PbArrayXY      = PbArrayLength * PbArrayHeight;
-    G4double PbArrayVolume  = (PbArrayXY * PbArrayDepth);
-    G4int nofPblineX        = (int) (PbArrayLength / brickLength + 0.5);
-    G4int nofPblineY        = (int) (PbArrayHeight / brickHeight + 0.5);
-    G4int nofPbXY           = (PbArrayXY / brickXY); 
-    G4int nofPbVol          = PbArrayVolume / brickVolume; 
-    G4int nofPblineZ        = nofPbVol / nofPbXY;
-    G4int nlayers           = nofPbVol / nofPblineX;
-    G4double worldSizeXY    = 1.2 * PbArrayXY;
-    G4double worldSizeZ     = 1.2 * PbArrayDepth;
+  // Geometry Parameters
+  G4double brickLength    = 20.0*cm;
+  G4double brickDepth     = 10.0*cm;
+  G4double brickHeight    = 5.0*cm;
+  G4double brickXY        = (brickLength * brickHeight);
+  G4double brickVolume    = (brickXY * brickDepth); 
+  G4double PbArrayLength  = 150.0*cm;
+  G4double PbArrayDepth   = 150.0*cm;
+  G4double PbArrayHeight  = 150.0*cm;
+  G4double PbArrayXY      = PbArrayLength * PbArrayHeight;
+  G4double PbArrayVolume  = (PbArrayXY * PbArrayDepth);
+  G4int nofPblineX        = (int) (PbArrayLength / brickDepth + 0.5);
+  G4int nofPblineY        = (int) (PbArrayHeight / brickHeight + 0.5);
+  G4int nofPblineZ        = (int) (PbArrayDepth / brickLength + 0.5);
+  G4int nofPbXY           = (PbArrayXY / brickXY); 
+  G4int nofPbVol          = PbArrayVolume / brickVolume; 
+  G4int nlayers           = nofPbVol / nofPblineX;
+  G4double worldSizeXY    = 1.2 * PbArrayXY;
+  G4double worldSizeZ     = 1.2 * PbArrayDepth;
+
+  //Get materials
+  G4Material* Pb               = G4Material::GetMaterial("G4_Pb");
+  G4Material* Be               = G4Material::GetMaterial("G4_Be");
+  G4Material* polyFilm         = G4Material::GetMaterial("G4_KAPTON");
+  G4Material* steel            = G4Material::GetMaterial("G4_STAINLESS-STEEL");
+  G4Material* Air              = G4Material::GetMaterial("G4_AIR");
+  G4Material* Argongas         = G4Material::GetMaterial("G4_Ar");
+  G4Material* Vac              = G4Material::GetMaterial("G4_Galactic");
+  G4Material* absorberMaterial = G4Material::GetMaterial("G4_Pb");
+  G4Material* gapMaterial      = G4Material::GetMaterial("liquidArgon");
+  G4Material* defaultMat       = G4Material::GetMaterial("G4_Galactic");  
 
    
-    unsigned i = 0, j = 0, k = 0, tmp1 = 0, tmp2 = 0;
-    G4double xnew= 0., ynew = 0., znew = 0.;
+  unsigned i = 0, j = 0, k = 0, tmp1 = 0, tmp2 = 0, tmp3 = 0;
+  G4double xnew= 0., ynew = 0., znew = 0.;
 
-    // World
-    G4VSolid* worldS = new G4Box("World", 0.5*worldSizeXY, 0.5*worldSizeXY, 0.5*worldSizeXY);  // size
-    G4VLogicalVolume* worldLV = new G4LogicalVolume(worldS, defaultMat, "WLog", 0, 0, 0);
-    G4VPhysicalVolume* worldPV = new G4PVPlacement(0,                 // no rotation
-                                                   G4ThreeVector(),   // at (0,0,0)
-                                                   worldLV,           // Logical Volume
-                                                   "wPhys",
-                                                   0,                 // Mother volume
-                                                   false,             // no boolean operation
-                                                   0,                 // copy number
-                                                   fCheckOverlaps);   // checking overlaps
+  // World
+  G4VSolid* worldS = new G4Box("World", 0.5*worldSizeXY, 0.5*worldSizeXY, 0.5*worldSizeXY);  // size
+  G4LogicalVolume* worldLV = new G4LogicalVolume(worldS, defaultMat, "WLog", 0, 0, 0);
+  G4VPhysicalVolume* worldPV = new G4PVPlacement(0,                 // no rotation
+						 G4ThreeVector(),   // at (0,0,0)
+						 worldLV,           // Logical Volume
+						 "wPhys",
+						 0,                 // Mother volume
+						 false,             // no boolean operation
+						 0,                 // copy number
+						 fCheckOverlaps);   // checking overlaps
    
-    // Lead Absorber
-    G4VSolid* PbArray = new G4Box("LeadArray", 0.5 * PbArrayXy, 0.5 * PbArrayXY, 0.5 * PbArraydepth);
-    G4VLogicalVolume* PbArrayLV = new G4LogicalVolume(PbArray, Pb, "LeadArray");
-    for (i = 0; i < nofPbVol; i++) 
+  // Lead Absorber
+  G4VSolid* PbArray = new G4Box("LeadArray", 0.5 * PbArrayXY, 0.5 * PbArrayXY, 0.5 * PbArrayDepth);
+  G4LogicalVolume* PbArrayLV = new G4LogicalVolume(PbArray, Pb, "LeadArray");
+  for (i = 0; i < nofPbVol; i++) 
     {
-        j = (j < nofPblineX) ? J++ : 0;
-        tmp1 = j;       tmp2 = i / nofPblineX;      tmp3 = i / nofPbXY;
-        xnew = tmp1 * brickLength;     ynew = tmp2 * brickHeight;    znew = tmp3 * brickDepth;
-        new G4PVPlacement(0, G4ThreeVectors(xnew, ynew, znew), PbArrayLV, 
-                          "PbArrayPhysical", worldLV, false,i,fCheckOverlaps);
+      j = (j < nofPblineX) ? j++ : 0;
+      tmp1 = j;       tmp2 = i / nofPblineX;      tmp3 = i / nofPbXY;
+      xnew = tmp1 * brickLength;     ynew = tmp2 * brickHeight;    znew = tmp3 * brickDepth;
+      new G4PVPlacement(0, G4ThreeVector(xnew, ynew, znew), PbArrayLV, 
+			"PbArrayPhysical", worldLV, false,i,fCheckOverlaps);
     }
 
 
-    // visualization attributes
-    worldLV->SetVisAttributes (G4VisAttributes::Invisible);
-    G4VisAttributes* simpleBoxVisAtt = new G4VisAttributes(G4Colour(1.0, 1.0, 1.0));
-    simpleBoxVisAtt->SetVisibility(true);
-    PbArrayLV->SetVisAttributes(simpleBoxVisAtt);
+  // visualization attributes
+  worldLV->SetVisAttributes (G4VisAttributes::Invisible);
+  G4VisAttributes* simpleBoxVisAtt = new G4VisAttributes(G4Colour(1.0, 1.0, 1.0));
+  simpleBoxVisAtt->SetVisibility(true);
+  PbArrayLV->SetVisAttributes(simpleBoxVisAtt);
 
-    return worldPV;
+  return worldPV;
 }
 
 
--- vegaPB01.cc
+++ vegaPB01.cc
@@ -1,35 +1,10 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
 // $Id:vegaPB01.cc 75215 2014-11-25 16:07:06Z ABHIJIT $
 //
 /// \file vegaPB01.cc
 /// \brief Main program 
 
 #include "DetectorConstruction.hh"
-#include "ActionInitialization.hh"
+//#include "ActionInitialization.hh"
 
 #ifdef G4MULTITHREADED
 #include "G4MTRunManager.hh"
@@ -109,20 +84,20 @@ int main(int argc,char** argv)
 
   // Set mandatory initialization classes
   //
-  DetectorConstruction* detConstruction = new DetectorConstruction();
-  runManager->SetUserInitialization(detConstruction);
-
-  /*
-  G4VModularPhysicsList* physicsList = new FTFP_BERT;
-  runManager->SetUserInitialization(physicsList);
+  DetectorConstruction* detConst = new DetectorConstruction();
+  runManager->SetUserInitialization(detConst);
+  
+  //  myPhysicsList* physicsList = new myPhysicsList();
+  //  runManager->SetUserInitialization(physicsList);
     
-  ActionInitialization* actionInitialization
-     = new ActionInitialization(detConstruction);
-  runManager->SetUserInitialization(actionInitialization);
-*/
+  //  myPrimaryGeneratorAction* myGen = new myPrimaryGeneratorAction();
+  //  runManager->SetUserAction(myGen);
+  
   // Initialize G4 kernel
   //
   runManager->Initialize();
+
+
   
 #ifdef G4VIS_USE
   // Initialize visualization
